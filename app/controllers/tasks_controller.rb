8dda6d235a06b0a878e356a5080f86a12e65dca8
Eleni Marli <elenimarli@yahoo.gr>
2024-01-23 16:03:34 +0100

    Implement 'As a user, I can remove a task' for 'Rails Task Manager' challenge

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -35,13 +35,15 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title, :details)
   end
 end
-
-
-# {"authenticity_token"=>"[FILTERED]", "task"=>{"title"=>"Do dishes",
-# "details"=>"Use old soap"}, "commit"=>"Create Task"}
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
 
+  # REMOVE A TASK
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'delete'
+
   # EDIT A TASK
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
   patch 'tasks/:id', to: 'tasks#update'
